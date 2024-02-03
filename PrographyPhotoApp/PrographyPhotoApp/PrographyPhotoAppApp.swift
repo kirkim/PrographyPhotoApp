b27ef6bd5f357c8c7b6b0d73052931b37bd90891
kirkim <kirimrlfla@naver.com>
2024-02-03 15:05:06 +0900

    feat: 프로젝트 생성

---
+++ PrographyPhotoApp/PrographyPhotoApp/PrographyPhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrographyPhotoAppApp.swift
+//  PrographyPhotoApp
+//
+//  Created by 김기림 on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PrographyPhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
