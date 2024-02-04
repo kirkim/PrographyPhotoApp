f181330d49c29c56f892cab813a40d81da5859dc
kirkim <kirimrlfla@naver.com>
2024-02-04 09:08:39 +0900

    feat: PhotoAppCoreData 로컬 패키지 생성 * Bookmark 포토 meta데이터 관리용 로컬DB 패키지

---
+++ LocalPackages/PhotoAppCoreData/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PhotoAppCoreData",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PhotoAppCoreData",
+            targets: ["PhotoAppCoreData"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PhotoAppCoreData"),
+        .testTarget(
+            name: "PhotoAppCoreDataTests",
+            dependencies: ["PhotoAppCoreData"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
